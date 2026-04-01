7c58949a7a3e7d68a78aa345feac0edd077e4e86
fatih1110 <vincenrafiq@gmail.com>
2026-04-01 14:35:58 +0700

    commit 3
    Signed-off-by: fatih1110 <vincenrafiq@gmail.com>

--- Looping_0064_3.cpp
+++ Looping_0064_3.cpp
@@ -12,4 +12,10 @@ int main(){
 
     rerata = (nilB+nilM)/2;
 
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak Lulus";
     
+    cout<<"Status kelulusan ="<<status<<" ,dengan nilai rata-rata ="<<rerata<<endl;
+}
